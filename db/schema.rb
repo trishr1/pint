b362a2c5a89f3cf1e53c4e0e9916ada2bdf8635e
Trish <trishrouse1@bigpond.com>
2015-11-25 16:50:52 +1000

    Day 21

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123233821) do
+ActiveRecord::Schema.define(version: 20151125045917) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
